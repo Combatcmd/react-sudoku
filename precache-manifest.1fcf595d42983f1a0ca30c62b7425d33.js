b283eee84560b6f8a8623f9597304a7c7024bed4
Combatcmd <combatcmd@gmail.com>
2020-05-16 16:33:02 +0600

    Updates

---
+++ precache-manifest.1fcf595d42983f1a0ca30c62b7425d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c406f866feccdc30aaa0790dd3f8813",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "02b700802b78b9b41552",
+    "url": "/react-sudoku/static/css/main.0b897270.chunk.css"
+  },
+  {
+    "revision": "fdbdb3c8d13b7e713ad9",
+    "url": "/react-sudoku/static/js/2.5e77f98c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react-sudoku/static/js/2.5e77f98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b700802b78b9b41552",
+    "url": "/react-sudoku/static/js/main.ff9a156c.chunk.js"
+  },
+  {
+    "revision": "a75df4807a0d823563ea",
+    "url": "/react-sudoku/static/js/runtime-main.a5b0a35c.js"
+  }
+]);
